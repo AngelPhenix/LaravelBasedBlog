0abf5ad86d35fece7565bdb62b9b31ae66da430c
AngelPhenix <breathstylebro@gmail.com>
2024-02-13 00:27:35 +0100

    Lesson on Routing for Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/** 
+ * LECON SUR LARAVEL
+ */
+
+Route::get('/test', function () {
+    return 'GET ROUTE';
+});
+
+Route::post('/store', function () {
+    return 'POST ROUTE';
+});
+
+Route::get('/articles/{id}/comments/{author?}', function ($id, $author = 'Jérémy') {
+    return $author . ' a écrit un commentaire sur l\'article numéro : '. $id;
+});
+
+Route::prefix('admin')->group(function() {
+    Route::get('users', function () {
+        return 'Ma user list';
+    });
+    
+    Route::get('articles', function () {
+        return 'Ma liste d\'article';
+    });
+    
+    Route::get('categories', function () {
+        return 'Ma liste de catégories';
+    });
+});
