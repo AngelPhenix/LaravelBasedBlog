edc60ad0b83fca2cb95c4920c7db5f77d16e9c4e
AngelPhenix <breathstylebro@gmail.com>
2024-02-14 01:08:05 +0100

    Added a navbar placeholder with blade

--- resources/views/folder/view.blade.php
+++ resources/views/folder/view.blade.php
@@ -1,1 +1,12 @@
-Vue dans le sous-dossier
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Ma page de view</title>
+  </head>
+  <body>
+    @include('navbar');
+    <p>PAGE VIEW</p>
+  </body>
+</html>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,12 @@
-{{ $id }}
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Ma page de test</title>
+  </head>
+  <body>
+    @include('navbar');
+    <p>PAGE TEST</p>
+  </body>
+</html>
