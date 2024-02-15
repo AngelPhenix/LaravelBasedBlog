4c85c780afb83c577b5f1eee5e8a8124871e2a51
AngelPhenix <breathstylebro@gmail.com>
2024-02-15 01:37:01 +0100

    Added new controller for article related to lesson 6 on CONTROLLERS

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,68 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-/** 
- * LECON SUR LARAVEL 001 ROUTING
- */
-
-
-Route::post('/store', function () {
-    return 'POST ROUTE';
-});
-
-Route::get('/articles/{id}/comments/{author?}', function ($id, $author = 'Jérémy') {
-    return $author . ' a écrit un commentaire sur l\'article numéro : '. $id;
-});
-
-Route::prefix('admin')->group(function() {
-    Route::get('users', function () {
-        return 'Ma user list';
-    });
-    
-    Route::get('articles', function () {
-        return 'Ma liste d\'article';
-    });
-    
-    Route::get('categories', function () {
-        return 'Ma liste de catégories';
-    });
-});
-
-/** 
- * LECON SUR LARAVEL 002 RESPONSES
- */
-
-Route::prefix('test')->group(function() {
-    Route::get('articles', function () {
-        return redirect('/try/articles');
-    });
-
-    Route::get('users', function () {
-        return response()->json([
-            'name' => 'Jérémy',
-            'age' => 33
-        ]);
-    });
-});
-
-/** 
- * LECON SUR LARAVEL 003 VIEWS
- */
-
-Route::get('/test', function () {
-    return view('test');
-});
-
-Route::get('/view', function () {
-    return view('folder.view');
-});
-
-Route::get('/test/{id}', function ($id) {
-    return view('test', [
-        'id' => $id
-    ]);
-});
+Route::resource('articles', ArticleController::class);
