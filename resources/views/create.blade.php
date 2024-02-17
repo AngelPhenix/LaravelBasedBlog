0c9c829a1cb87e6a4b9f447fae10f8f5a5079944
AngelPhenix <breathstylebro@gmail.com>
2024-02-17 15:33:16 +0100

    Added flash messages for errors when rules are not meant

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,6 +1,17 @@
 @extends('base')
 
 @section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="/articles" method="post" class="form-example">
         @csrf
         <div class="form-example">
