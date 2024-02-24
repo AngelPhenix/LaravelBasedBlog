0fc413f12b401022f158d47257c77e6b16ddee61
AngelPhenix <breathstylebro@gmail.com>
2024-02-24 01:14:33 +0100

    Added new CTAs in the navbar

--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="/home">
         <i class="fa-solid fa-house-chimney"></i>
         Accueil</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,26 +9,23 @@
       <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link active" href="#">Link
+            <a class="nav-link active" href="/articles">Articles
             </a>
           </li>
         </ul>
         <ul class="navbar-nav ml-auto">
           @if (Auth::user())
               <li class="nav-item">
-                <a class="nav-link active" href="#">Déconnexion
-                </a>
+                <form action="POST" action="/logout">
+                  @csrf
+                  <button type="submit" class="btn">Déconnexion</button>
+                </form>
               </li>           
           @else
                 <li class="nav-item">
-                  <a class="nav-link active" href="#">Se connecter
-                  </a>
+                  <a class="nav-link active" href="/login">Se connecter</a>
                 </li>
           @endif
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Link
-            </a>
-          </li>
         </ul>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,9 @@ use App\Http\Controllers\MainController;
 |
 */
 
-Route::get('/', [MainController::class, 'home']);
+Route::get('/', [MainController::class, 'home'])->name('home');
 
 Route::get('/articles', [MainController::class, 'index'])->name('articles');
 Route::get('/articles/{slug}', [MainController::class, 'show'])->name('article');
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
