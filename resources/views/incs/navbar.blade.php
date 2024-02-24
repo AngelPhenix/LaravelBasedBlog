dffd12754ada5293b7363e910eb1d569845199af
AngelPhenix <breathstylebro@gmail.com>
2024-02-24 01:21:12 +0100

    Added new routes to the CTAs in the navbar for it to redirect properly

--- resources/views/incs/navbar.blade.php
+++ resources/views/incs/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/home">
+      <a class="navbar-brand" href="{{ route('home') }}">
         <i class="fa-solid fa-house-chimney"></i>
         Accueil</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,21 +9,21 @@
       <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <a class="nav-link active" href="/articles">Articles
+            <a class="nav-link active" href="{{ route('articles') }}">Articles
             </a>
           </li>
         </ul>
         <ul class="navbar-nav ml-auto">
           @if (Auth::user())
               <li class="nav-item">
-                <form action="POST" action="/logout">
+                <form action="POST" action="{{ route('logout') }}">
                   @csrf
                   <button type="submit" class="btn">Déconnexion</button>
                 </form>
-              </li>           
+              </li>         
           @else
                 <li class="nav-item">
-                  <a class="nav-link active" href="/login">Se connecter</a>
+                  <a class="nav-link active" href="{{ route('login') }}">Se connecter</a>
                 </li>
           @endif
         </ul>
